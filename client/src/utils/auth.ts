84416575504434b4f0a297d148a3c366807e162c
Shelle <shelle@Juleens-MacBook-Pro.local>
2025-04-21 18:02:18 -0500

    adds in server routes

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,53 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken()) as JwtPayload;
+    
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+      const currentTime = Date.now()/1000;
+     if (decodedToken.exp && decodedToken.exp < currentTime)
+        return true;
+     }
+
+    catch (error) {
+      console.error('Error decoding token:', error);
+      return true; // If there's an error, assume the token is expired
+  }
+    return false;
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('user') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('user', idToken)
     // TODO: redirect to the home page
+    window.location.assign('/');
+
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('user');
     // TODO: redirect to the login page
+    window.location.assign('login');
   }
 }
 
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,6 +4,24 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: {username},
+  });
+  if (!user) {
+    return res.status(401).json({ message : 'invalid username'});
+  }
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message : 'invalid password'});
+  }
+
+  const secretKey =  process.env.JWT_SECRET_KEY || '';
+  const token = jwt.sign({ username}, secretKey, { expiresIn: '1h'});
+  return res.json ({ token}) ; 
+  
   // TODO: If the user exists and the password is correct, return a JWT token
 };
 
